1f56553568acce5ec6b786b0d7f44c1b9ba8cc21
spicelion1802 <adivsr@seas.upenn.edu>
2025-06-02 20:30:03 -0400

    create ll

--- linked_list.c
+++ linked_list.c
@@ -5,3 +5,39 @@
 //
 static void * (*malloc_fptr)(size_t size) = NULL;
 static void   (*free_fptr)(void* addr)    = NULL; 
+
+// Creates a new linked_list.
+// PRECONDITION: Register malloc() and free() functions via the
+//               linked_list_register_malloc() and 
+//               linked_list_register_free() functions.
+// POSTCONDITION: An empty linked_list has its head point to NULL.
+// Returns a new linked_list on success, NULL on failure.
+//
+struct linked_list * linked_list_create(void){
+    struct linked_list * ll = malloc_fptr(sizeof(struct linked_list));
+    ll->head->data = NULL;
+    ll->head->next = NULL;
+    return ll;
+}
+
+// Returns the size of a linked_list.
+// \param ll : Pointer to linked_list.
+// Returns size on success, SIZE_MAX on failure.
+//
+size_t linked_list_size(struct linked_list * ll){
+    if(ll->head->next==NULL){
+        return 0;
+    }
+
+}
+
+// Inserts an element at the end of the linked_list.
+// \param ll   : Pointer to linked_list.
+// \param data : Data to insert.
+// Returns TRUE on success, FALSE otherwise.
+//
+bool linked_list_insert_end(struct linked_list * ll,unsigned int data){
+    while(ll->head!=NULL){
+        head->
+    }
+    }
